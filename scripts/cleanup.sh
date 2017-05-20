21b66820c2d75009bd1e59f95b572530984ad93b
Wojciech Sciesinski <wojciech@sciesinski.net>
2017-05-21 01:47:43 +0200

    Build for Ubuntu 14.04.5 LTS corrected

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,11 +3,17 @@
 # Uninstall Ansible and remove PPA.
 apt-get -y remove --purge ansible
 apt-add-repository --remove ppa:ansible/ansible
-apt-get autoremove
 apt-get update
+apt-get -y autoremove
+apt-get clean
+
+#Remove older linux kernels - source: https://askubuntu.com/posts/254585/revisions
+#dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
+echo $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs sudo apt-get -y purge
 
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
+rm -r /var/log/*.gz
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
---
+++ scripts/dist-upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eux
+
+# Install Ansible repository.
+apt-get -y dist-upgrade
